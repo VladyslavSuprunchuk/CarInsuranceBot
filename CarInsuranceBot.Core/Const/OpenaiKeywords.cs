a7ccbdebd999e6b09463a2cb3d36d143591d61fd
vladyslav <vlaz1234567@gmail.com>
2024-06-10 11:50:24 +0300

    Update message for gpt request

--- CarInsuranceBot.Core/Const/OpenaiKeywords.cs
+++ CarInsuranceBot.Core/Const/OpenaiKeywords.cs
@@ -27,7 +27,7 @@
                                                      """;
 
         public const string FillTemplateRequest = "Fill the following template with random data";
-        public const string LiveChattingRequest = "Please execute following request {0}, if it is related to the topic of Identity and Vehicle Records, or any relevant matters, proceed accordingly. If not, just respond that the request is not appropriate.";
+        public const string LiveChattingRequest = "Please execute following request {0}, if it is related to the topic of Insurance and Vehicle Ownership, or any relevant matters, proceed accordingly. If not, just respond that the request is not appropriate.";
 
         public const string ModelName = "gpt-3.5-turbo-instruct";
     }
